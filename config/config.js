a053d187f0358a553b121dead321306ee662f289
Vinicius Leprevost <vinilep@hotmail.com>
2024-11-13 14:50:03 -0500

    new db config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3001,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://zaingova:zvqMaMlgYYdl75pC@cluster0.yd67o.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster0" ||
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://CanTrade:CanTrade@1324@cluster0.pnvyo.mongodb.net/CanTrade?retryWrites=true&w=majority&appName=Cluster0" ||
         process.env.MONGO_HOST ||
         'mongodb://' + (process.env.IP || 'localhost') + ':' +
         (process.env.MONGO_PORT || '27017') +
