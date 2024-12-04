1ca52373f7a8203bee97cc891105d8d74c74aa86
Vinicius Leprevost <vinilep@hotmail.com>
2024-12-04 17:22:21 -0500

    favorites now works

--- client/components/ListingCard/ListingCard.jsx
+++ client/components/ListingCard/ListingCard.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useLocation } from 'react-router';
 import { useAuth } from '../../helpers/auth-context';
 import { useCart } from '../../helpers/CartContext';
+import { useFavorites } from '../../helpers/FavoritesContext';
 import {
   Card,
   CardContent,
@@ -16,13 +17,15 @@ import {
 } from '@mui/material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import './ListingCard.css'; // Import the external CSS file
 
-const ListingCard = ({ listing }) => {
+const ListingCard = ({ listing, isFavorite }) => {
   const { isAuthenticated } = useAuth();
   const { addToCart } = useCart();
+  const { addToFavorites, removeFromFavorites } = useFavorites();
   const location = useLocation();
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
@@ -36,6 +39,18 @@ const ListingCard = ({ listing }) => {
   const handleAddToCart = () => {
     addToCart(listing);
     setSnackbarMessage(`${listing.title} added to cart!`);
+    setSnackbarOpen(true);
+  };
+
+  const handleAddToFavorites = () => {
+    addToFavorites(listing);
+    setSnackbarMessage(`${listing.title} added to favorites!`);
+    setSnackbarOpen(true);
+  };
+
+  const handleRemoveFromFavorites = () => {
+    removeFromFavorites(listing._id);
+    setSnackbarMessage(`${listing.title} removed from favorites!`);
     setSnackbarOpen(true);
   };
 
@@ -83,15 +98,11 @@ const ListingCard = ({ listing }) => {
           )}
           {listing.postedBy?.name && (
             <Typography variant="body2" color="textSecondary" className="listing-posted-by">
-              Posted by: {listing.postedBy.name
-                .toLowerCase()
-                .split(' ')
-                .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(' ')}
+              Posted by: {listing.postedBy.name}
             </Typography>
           )}
 
-          {isAuthenticated && showPublicButtons && (
+          {isAuthenticated && (
             <Box className="fab-container">
               <Tooltip title="Add to Cart" arrow>
                 <Fab
@@ -104,14 +115,14 @@ const ListingCard = ({ listing }) => {
                   <AddShoppingCartIcon />
                 </Fab>
               </Tooltip>
-              <Tooltip title="Add to Favorites" arrow>
+              <Tooltip title={isFavorite ? "Remove from Favorites" : "Add to Favorites"} arrow>
                 <Fab
                   color="secondary"
                   size="small"
-                  onClick={() => console.log(`Added ${listing.title} to favorites!`)}
-                  aria-label="Add to Favorites"
+                  onClick={isFavorite ? handleRemoveFromFavorites : handleAddToFavorites}
+                  aria-label={isFavorite ? "Remove from Favorites" : "Add to Favorites"}
                 >
-                  <FavoriteIcon />
+                  {isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
                 </Fab>
               </Tooltip>
             </Box>
@@ -144,8 +155,6 @@ const ListingCard = ({ listing }) => {
           )}
         </CardContent>
       </Card>
-
-      {/* Snackbar Component */}
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={3000}
@@ -161,7 +170,8 @@ const ListingCard = ({ listing }) => {
 };
 
 ListingCard.propTypes = {
-  listing: PropTypes.object,
+  listing: PropTypes.object.isRequired,
+  isFavorite: PropTypes.bool,
 };
 
 export default ListingCard;
