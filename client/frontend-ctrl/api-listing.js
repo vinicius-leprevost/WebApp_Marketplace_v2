869b40734c270c99de11a2379776fc6e5dbbcae8
Vinicius Leprevost <vinilep@hotmail.com>
2024-12-10 21:31:11 -0500

    Front-end deployment test

--- client/frontend-ctrl/api-listing.js
+++ client/frontend-ctrl/api-listing.js
@@ -1,6 +1,8 @@
+import apiUrl from './config';
+
 const create = async (formData) => {
     try {
-        let response = await fetch('/api/listings/', {
+        let response = await fetch(`${apiUrl}/api/listings/`, {
             method: 'POST',
             body: formData,
         });
@@ -12,7 +14,7 @@ const create = async (formData) => {
 
 const list = async (signal) => {
     try {
-        let response = await fetch('/api/listings/', {
+        let response = await fetch(`${apiUrl}/api/listings/`, {
             method: 'GET',
             signal: signal,
         })
@@ -24,7 +26,7 @@ const list = async (signal) => {
 
 const read = async (params, credentials, signal) => {
     try {
-        let response = await fetch('/api/listings/' + params.userId, {
+        let response = await fetch(`${apiUrl}/api/listings/` + params.userId, {
             method: 'GET',
             signal: signal,
             headers: {
@@ -41,7 +43,7 @@ const read = async (params, credentials, signal) => {
 
 const update = async (params, user) => {
     try {
-        let response = await fetch('/api/listings/' + params.listingId, {
+        let response = await fetch(`${apiUrl}/api/listings/` + params.listingId, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -59,7 +61,7 @@ const remove = async (params) => {
     try {
         console.log("Listing ID:", params.listingId); // Log listingId
 
-        const response = await fetch('/api/listings/' + params.listingId, {
+        const response = await fetch(`${apiUrl}/api/listings/` + params.listingId, {
             method: 'DELETE',
             headers: {
                 'Accept': 'application/json',
--- client/frontend-ctrl/api-user.js
+++ client/frontend-ctrl/api-user.js
@@ -1,6 +1,8 @@
+import apiUrl from './config';
+
 const create = async (user) => {
     try {
-        let response = await fetch('/api/users/', {
+        let response = await fetch(`${apiUrl}/api/users/`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -16,7 +18,7 @@ const create = async (user) => {
 
 const list = async (signal) => {
     try {
-        let response = await fetch('/api/users/', {
+        let response = await fetch(`${apiUrl}/api/users/`, {
             method: 'GET',
             signal: signal,
         })
@@ -28,7 +30,7 @@ const list = async (signal) => {
 
 const read = async (params, credentials, signal) => {
     try {
-        let response = await fetch('/api/users/' + params.userId, {
+        let response = await fetch(`${apiUrl}/api/users/` + params.userId, {
             method: 'GET',
             signal: signal,
             headers: {
@@ -45,7 +47,7 @@ const read = async (params, credentials, signal) => {
 
 const update = async (params, credentials, user) => {
     try {
-        let response = await fetch('/api/users/' + params.userId, {
+        let response = await fetch(`${apiUrl}/api/users/` + params.userId, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -62,7 +64,7 @@ const update = async (params, credentials, user) => {
 
 const remove = async (params, credentials) => {
     try {
-        let response = await fetch('/api/users/' + params.userId, {
+        let response = await fetch(`${apiUrl}/api/users/` + params.userId, {
             method: 'DELETE',
             headers: {
                 'Accept': 'application/json',
